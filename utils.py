7483ee3572a8ab414e71ef529a9448b29c96d74e
felipeimpieri <108438752+felipeimpieri@users.noreply.github.com>
2025-08-19 11:41:29 -0300

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def interval_per_year(interval: str) -> int:
-    # Aproximaciones razonables
-    if interval == "1d":
-        return 252
-    if interval == "1h":
-        return int(252 * 6.5)  # horas promedio por sesión en EEUU
-    if interval == "1wk":
-        return 52
-    return 252
